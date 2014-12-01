1e45e378027bd0be8922c6687701ab25cb2707ce
Asus Pc <Asus Pc@Asus>
2014-12-01 11:17:13 -0500

    Versión 2

---
+++ ProyectoFinal-war/src/java/ControlWeb/UsuarioMB.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ControlWeb;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author Asus Pc
+ */
+@ManagedBean
+@SessionScoped
+public class UsuarioMB {
+
+    /**
+     * Creates a new instance of UsuarioMB
+     */
+    public UsuarioMB() {
+    }
+    
+}
